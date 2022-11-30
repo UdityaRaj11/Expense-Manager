9452c0036d7997dfd7629d20e3145c84afb8b815
Uditya Raj <udityaraj2002@gmail.com>
2022-11-30 12:03:43 +0530

    optimized Code

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,6 @@
 import '../models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transaction;
@@ -35,41 +35,8 @@ class TransactionList extends StatelessWidget {
             })
           : ListView.builder(
               itemBuilder: (jj, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 4),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text('\₹${transaction[index].amount}'),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      transaction[index].title,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transaction[index].date),
-                    ),
-                    trailing: MediaQuery.of(context).size.width > 360
-                        ? FlatButton.icon(
-                            icon: Icon(Icons.delete),
-                            label: Text('Delete'),
-                            textColor: Theme.of(context).errorColor,
-                            onPressed: () => deleteTx(transaction[index].id),
-                          )
-                        : IconButton(
-                            icon: Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () => deleteTx(transaction[index].id),
-                          ),
-                  ),
-                );
+                return TransactionItem(
+                    transaction: transaction[index], deleteTx: deleteTx);
               },
               itemCount: transaction.length,
             ),
